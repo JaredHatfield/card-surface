48733979ae5c9f16902d6eaf3e27a652462896c0
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-02-05 21:58:01 +0000

    Continued to set up the project.

---
+++ trunk/card-surface/card-game/Card.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Card.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>A single playing card.</summary>
+namespace CardGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A single playing card.
+    /// </summary>
+    public class Card
+    {
+        // TODO: Implement Card
+    }
+}
---
+++ trunk/card-surface/card-game/Deck.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Deck.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>A collection of cards that the game uses.</summary>
+namespace CardGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A collection of cards that the game uses.
+    /// </summary>
+    public class Deck
+    {
+        // TODO: Implement Deck
+    }
+}
