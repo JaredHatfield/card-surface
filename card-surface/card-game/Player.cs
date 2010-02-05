2f1ee7ef90e717cdfd20e52a73df95e6b208f14b
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-02-05 21:58:01 +0000

    Continued to set up the project.

---
+++ card-surface/card-game/Player.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Player.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>A player in the game.</summary>
+namespace CardGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A player in the game.
+    /// </summary>
+    public class Player
+    {
+        // TODO: Implement Player
+    }
+}
