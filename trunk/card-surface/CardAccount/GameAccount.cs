cf71d068d482984bd2839653ce0982a99c246fb9
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-02-09 19:52:53 +0000

    Work in progress.

---
+++ trunk/card-surface/CardAccount/GameAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace CardAccount
+{
+    class GameAccount
+    {
+        private string username;
+
+        private string password;
+
+        private string profileImage;
+
+        private int balance;
+
+        private int gamesPlayed;
+
+
+        public string Username
+        {
+            get { return this.username; }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+        }
+
+    }
+}
