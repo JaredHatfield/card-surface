2380f53b5ef7d3c18ff4e681bbd9b3424e2991b2
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-02-11 23:21:37 +0000

    Added the IGameController and passed an instance of the game controller to the classes that require access.

--- card-surface/CardCommunication/CommunicationController.cs
+++ card-surface/CardCommunication/CommunicationController.cs
@@ -8,11 +8,25 @@ namespace CardCommunication
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using CardGame;
 
     /// <summary>
     /// The base class for the communication controller.
     /// </summary>
     public class CommunicationController
     {
+        /// <summary>
+        /// The GameController that the communication controller is able to interact with.
+        /// </summary>
+        private IGameController gameController;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommunicationController"/> class.
+        /// </summary>
+        /// <param name="gameController">The game controller.</param>
+        internal CommunicationController(IGameController gameController)
+        {
+            this.gameController = gameController;
+        }
     }
 }
