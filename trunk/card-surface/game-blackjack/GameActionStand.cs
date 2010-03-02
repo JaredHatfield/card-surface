fdc1c87e1021bc356b0a6773dab155a942227dae
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-03-02 23:43:06 +0000

    Implemented abstract GameAction class that allows a games to have custom player actions to be created that modify the current game state.

---
+++ trunk/card-surface/game-blackjack/GameActionStand.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="GameActionStand.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>The stand action for blackjack.</summary>
+namespace GameBlackjack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using CardGame;
+
+    /// <summary>
+    /// The stand action for blackjack.
+    /// </summary>
+    internal class GameActionStand : GameAction
+    {
+        /// <summary>
+        /// Gets this actions name.
+        /// </summary>
+        /// <value>The actions name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "stand";
+            }
+        }
+
+        /// <summary>
+        /// Perform the action on the specified game.
+        /// </summary>
+        /// <param name="game">The game to modify.</param>
+        /// <param name="player">The player that triggered this action.</param>
+        public override void Action(Game game, Guid player)
+        {
+            Blackjack blackjack = (Blackjack)game;
+
+            // TODO: GameActionHit - implement a player standing in the game
+        }
+    }
+}
