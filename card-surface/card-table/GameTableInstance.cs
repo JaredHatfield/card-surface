e724f26a3bb01162fc0bb2279e7595f7b68574a8
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-03-04 04:04:23 +0000

    Added the GameTableInstance class that extends the Game class specifically for a the Table. This class will be responsible for keeping the current game state and using the TableCommunicationController to talk to the server.

---
+++ card-surface/card-table/GameTableInstance.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="GameTableInstance.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>The game instance on the table.</summary>
+namespace CardTable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using CardCommunication;
+    using CardGame;
+
+    /// <summary>
+    /// The game instance on the table.
+    /// </summary>
+    internal class GameTableInstance : Game
+    {
+        /// <summary>
+        /// The TableCommunicationController that coordinates the communication to the server.
+        /// </summary>
+        private TableCommunicationController tableCommunicationController;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameTableInstance"/> class.
+        /// </summary>
+        internal GameTableInstance()
+            : base()
+        {
+            this.tableCommunicationController = new TableCommunicationController();
+
+            // TODO: Customize this class so that it does something useful.
+        }
+    }
+}
