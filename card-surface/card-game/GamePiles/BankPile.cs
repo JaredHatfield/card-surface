c3f5453965eb6c072a00ae0a13a46b80d603e52a
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-03-23 20:28:56 +0000

    Started working on issue 22, but the specific logic required by this feature requires modifications to the Game class to insure all of the requirements are met.

---
+++ card-surface/card-game/GamePiles/BankPile.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="BankPile.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>A pile of cards.</summary>
+namespace CardGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A pile of chips that the user can access money they have placed on the table.
+    /// </summary>
+    public class BankPile : CardPile
+    {
+        // TODO: Implement all of the logic that is unique to the BankPile
+    }
+}
