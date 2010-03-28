bf6dbfd66cd664b573884d624b4b4a63402ad26c
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-03-28 15:24:17 +0000

    Added game actions for awarding chips and clearing the board of chips.  Unlike the other actions these will not be called by the player, rather will be called when a certain conditions of the game are met and require these actions to be taken. These are just the shell for these algorithms.  They still need to be implemented and integrated into Blackjack.

---
+++ card-surface/game-blackjack/GameActionAwardChips.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="GameActionAwardChips.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>Clears the table of chips.</summary>
+namespace GameBlackjack
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using CardGame;
+
+    /// <summary>
+    /// Clears the table of chips.
+    /// </summary>
+    internal class GameActionAwardChips : GameAction
+    {
+        /// <summary>
+        /// Gets this actions name.
+        /// </summary>
+        /// <value>The actions name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "AwardChips";
+            }
+        }
+
+        /// <summary>
+        /// Perform the action on the specified game.
+        /// </summary>
+        /// <param name="game">The game to modify.</param>
+        /// <param name="player">The player that triggered this action.</param>
+        /// <returns>True if the action was successful; otherwise false.</returns>
+        public override bool Action(Game game, string player)
+        {
+            Blackjack blackjack = game as Blackjack;
+
+            // TODO: Write algorithm to award players chips after the game is over.
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether [is executable by player] [the specified game].
+        /// </summary>
+        /// <param name="game">The game to check.</param>
+        /// <param name="player">The player to modify.</param>
+        /// <returns>
+        /// <c>true</c> if [is executable by player] [the specified game]; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsExecutableByPlayer(Game game, Player player)
+        {
+            // TODO: Check to see if the game is over and chips can be awarded.
+            return false;
+        }
+    }
+}
