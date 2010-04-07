2cb5ae723a2596276e79fcbdcd77c2f624bd6b99
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-04-07 12:41:20 +0000

    Created a very simple version of the game playing area using XAML surface elements.

---
+++ trunk/card-surface/card-table/SurfacePlayerArea.xaml.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="SurfacePlayerArea.xaml.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>Interaction logic for SurfacePlayerArea.xaml</summary>
+namespace CardTable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+    using Microsoft.Surface;
+    using Microsoft.Surface.Presentation;
+    using Microsoft.Surface.Presentation.Controls;
+
+    /// <summary>
+    /// Interaction logic for SurfacePlayerArea.xaml
+    /// </summary>
+    public partial class SurfacePlayerArea : SurfaceUserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurfacePlayerArea"/> class.
+        /// </summary>
+        public SurfacePlayerArea()
+        {
+            InitializeComponent();
+        }
+    }
+}
