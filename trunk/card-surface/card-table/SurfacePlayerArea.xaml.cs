bab040b7aacdc2d6e05ceda45d7997fc90a545ba
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-04-10 21:17:16 +0000

    Fixed the Table so that objects can be moved from pile to pile without causing problems. If a amage is added to a LibraryStack or LibraryBar it can be moved to any pile. This also includes a workaround to prevent objects from leaving behind an inactive version of themselves when they are moved out of a LibraryBar.

--- trunk/card-surface/card-table/SurfacePlayerArea.xaml.cs
+++ trunk/card-surface/card-table/SurfacePlayerArea.xaml.cs
@@ -32,6 +32,26 @@ namespace CardTable
         public SurfacePlayerArea()
         {
             InitializeComponent();
+
+            // We want to always assume objects are being moved
+            // This insures that when we move out of a LibraryBar we do not get an inactive object left behind
+            SurfaceDragDrop.AddPreviewDropHandler(this.bank, this.OnPreviewDrop);
+            SurfaceDragDrop.AddPreviewDropHandler(this.hand, this.OnPreviewDrop);
+            SurfaceDragDrop.AddPreviewDropHandler(this.chip0, this.OnPreviewDrop);
+            SurfaceDragDrop.AddPreviewDropHandler(this.chip1, this.OnPreviewDrop);
+            SurfaceDragDrop.AddPreviewDropHandler(this.card0, this.OnPreviewDrop);
+            SurfaceDragDrop.AddPreviewDropHandler(this.card1, this.OnPreviewDrop);
+        }
+
+        /// <summary>
+        /// Called when PreviewDropHandler is called.
+        /// This method forces every drop to be a move.  This specifically prevents a LibraryBar for leaving behind inactive objects.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="Microsoft.Surface.Presentation.SurfaceDragDropEventArgs"/> instance containing the event data.</param>
+        private void OnPreviewDrop(object sender, SurfaceDragDropEventArgs e)
+        {
+            e.Effects = DragDropEffects.Move;
         }
     }
 }
