f7a311f5ec73edd557376fc59286554db2239713
aaron7baker <aaron7baker@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-04-10 18:18:40 +0000

    non abstract Game object.

---
+++ card-surface/CardCommunication/GameObject/GameMessage.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="GameMessage.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>A game object representing the game.</summary>
+namespace CardCommunication.GameObject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using CardGame;
+
+    [Serializable] public class GameMessage:Game
+    {
+        public GameMessage()
+        {
+        }
+    }
+}
