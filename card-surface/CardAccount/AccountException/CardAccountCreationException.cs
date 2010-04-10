6687bf67ae05473259eea11480c5727e1f06316f
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-04-10 19:22:53 +0000

    Fixed issue 28.

---
+++ card-surface/CardAccount/AccountException/CardAccountCreationException.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="CardAccountCreationException.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>An exception thrown by CardAccountCreationException.</summary>
+namespace CardAccount.AccountException
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// An exception that indicates a new account could not be created.
+    /// </summary>
+    public class CardAccountCreationException : CardAccountException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CardAccountCreationException"/> class.
+        /// </summary>
+        public CardAccountCreationException()
+            : base("CardAccount: New account could not be created.")
+        {
+        }
+    }
+}
