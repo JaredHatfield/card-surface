af8a21469efd3aa9843d314c64cbba147def4324
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-04-12 15:19:52 +0000

    Fixed issue 67.

---
+++ card-surface/card-game/GameException/CardGameSeatNotSittableException.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="CardGameSeatNotSittableException.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>An exception thrown by CardGame when someone tries to sit in a Seat they are not allowed to.</summary>
+namespace CardGame.GameException
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// An exception thrown by CardGame when someone tries to sit in a Seat they are not allowed to.
+    /// </summary>
+    public class CardGameSeatNotSittableException : CardGameException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CardGameSeatNotSittableException"/> class.
+        /// </summary>
+        public CardGameSeatNotSittableException()
+            : base("Players are not allowed to sit in this Seat.")
+        {
+        }
+    }
+}
