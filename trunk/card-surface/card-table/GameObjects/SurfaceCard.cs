b1d847a3dc6e4d0b5a252d2ea76507a9e421b2b2
jjhatf02 <jjhatf02@bcda4484-046f-11df-9f17-9dbddc73297d>
2010-04-20 20:47:21 +0000

    Started work on the SurfaceChip, CurfaceCard, SurfaceChipFactory, and SurfaceCardFactory.

---
+++ trunk/card-surface/card-table/GameObjects/SurfaceCard.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="SurfaceCard.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>Implements the SurfaceCard.</summary>
+namespace CardTable.GameObjects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using CardGame;
+    using Microsoft.Surface;
+    using Microsoft.Surface.Presentation;
+    using Microsoft.Surface.Presentation.Controls;
+
+    /// <summary>
+    /// The surface card.
+    /// </summary>
+    internal class SurfaceCard : Card
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurfaceCard"/> class.
+        /// </summary>
+        /// <param name="id">The card's id.</param>
+        /// <param name="suit">The card's suit.</param>
+        /// <param name="face">The card's face.</param>
+        /// <param name="status">The card's status.</param>
+        internal SurfaceCard(Guid id, CardSuit suit, CardFace face, CardStatus status)
+            : base(id, suit, face, status)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurfaceCard"/> class.
+        /// </summary>
+        /// <param name="suit">The card's suit.</param>
+        /// <param name="face">The card's face.</param>
+        /// <param name="status">The card's status.</param>
+        internal SurfaceCard(CardSuit suit, CardFace face, CardStatus status)
+            : base(suit, face, status)
+        {
+        }
+    }
+}
